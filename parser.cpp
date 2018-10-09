65158c8f8b6795644e4091f3fdb6ce9ef5ed790b
Caleb Gartner <calebgtnr+github@gmail.com>
2018-10-09 14:57:43 -0600

    Added CMake file Added main parser source file Added recursive function stubs Added TODOs

---
+++ parser.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+void parse() {
+
+}
+
+void expr() {
+
+}
+
+void term() {
+
+}
+
+void factor() {
+
+}
+
+void number() {
+
+}
+
+int main() {
+    // TODO get input, print out expression to parse, pass to parse() function
+    std::cout << "Begin parse: " << std::endl;
+    return 0;
+}
