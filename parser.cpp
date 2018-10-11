a4b153665dc2ff9bdae504dc2e97b255ae085b11
Caleb Gartner <calebgtnr+github@gmail.com>
2018-10-11 14:13:42 -0600

    Added EOL checks for each expression Improved command-line argument parsing, reduced to if=/infile= and of=/outfile= args Added parser output to specified/default outfile

--- parser.cpp
+++ parser.cpp
@@ -2,18 +2,12 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <cctype>
 #include <string>
 #include <deque>
-#include <set>
 
 
 namespace rdp {
-
-    // TODO make static ?
-    std::set<char> digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    char token;
-    std::deque<char> expression;
-    std::string outfile = "rdp-output.txt";
 
     void get_token();
     void parse(std::string arithmetic_expr);
@@ -23,11 +17,22 @@ namespace rdp {
     void number();
     void digit();
 
+    // TODO make static ?
+    char token;
+    std::deque<char> expression;
+    const char * output_filename = "rdp-output.txt";  // TODO use buffer for both streams?
+    std::ofstream outfile;
+
     void get_token() {
-        std::cout << std::endl << "<gettoken> ";
-        token = expression.front();
-        expression.pop_front();
-        // check for '\0' here?
+        do {
+            token = expression.front();
+            expression.pop_front();
+        } while (std::isspace(token));  // eats up spaces
+
+        if (token != '\0') {
+            std::cout << "\n<gettoken> " << token << " ";
+            outfile << "\n<gettoken> " << token << " ";
+        }
     }
 
     void parse(std::string arithmetic_expr) {
@@ -35,14 +40,24 @@ namespace rdp {
         std::copy(arithmetic_expr.begin(), arithmetic_expr.end(), std::back_inserter(expression));
         expression.push_back('\0');
 
-        std::cout << "Begin parse: " << arithmetic_expr << std::endl;
-        std::cout << "<parse> ";
+        outfile = std::ofstream(output_filename, std::ios::app);
+
+        std::cout << "\nArithmetic Expression: " << arithmetic_expr << "\n<parse> ";
+        outfile << "\nArithmetic Expression: " << arithmetic_expr << "\n<parse> ";
         get_token();
-        expr();
+        try {
+            expr();
+            std::cout << "\nEnd parse: success!\n";
+            outfile << "\nEnd parse: success!\n";
+        } catch (const std::logic_error& err){  // TODO more explicit error msgs - give source of error (token)
+            std::cout << "\nEnd parse: failure: " << err.what() << " '" << arithmetic_expr << "'\n";
+            outfile << "\nEnd parse: failure: " << err.what() << " '" << arithmetic_expr << "'\n";
+        }
     }
 
     void expr() {
         std::cout << "<expr> ";
+        outfile << "<expr> ";
         term();
         while (token == '+') {
             get_token();
@@ -52,6 +67,7 @@ namespace rdp {
 
     void term() {
         std::cout << "<term> ";
+        outfile << "<term> ";
         factor();
         while (token == '*') {
             get_token();
@@ -61,13 +77,14 @@ namespace rdp {
 
     void factor() {
         std::cout << "<factor> ";
+        outfile << "<factor> ";
         if (token == '(') {
             get_token();
             expr();
             if (token == ')') {
                 get_token();
             } else {
-                throw std::logic_error("");  // TODO err msg
+                throw std::logic_error("invalid arithmetic expression");
             }
         } else {
             number();
@@ -76,16 +93,16 @@ namespace rdp {
 
     void number() {
         std::cout << "<number> ";
+        outfile << "<number> ";
         digit();  // only parses single digits
     }
 
     void digit() {
-        std::cout << "<digit> ";
-        if (digits.count(token) == 1) {
+        outfile << "<digit> ";
+        if (std::isdigit(token)) {
             get_token();
         } else {
-            // TODO check for '\0' char as well . . .
-            throw std::logic_error("");  // TODO err msg
+            throw std::logic_error("invalid arithmetic expression");
         }
     }
 }
@@ -93,32 +110,44 @@ namespace rdp {
 
 int main(int argc, char** argv)
 {
-    std::string expression_line;  // TODO make static ?
+    if (argc > 3) {
+        throw std::invalid_argument("too many arguments; please try again");
+    }
+    std::string expression_line;
+    const char* input_file = nullptr;
 
-    if (argc > 1) {
-        if (std::strcmp(argv[1], "-f") == 0 or std::strcmp(argv[1], "--file") == 0) {
-            if (argc < 3 or std::strcmp(argv[2], "") == 0) {
-                throw std::invalid_argument("file option '-f' or '--file' was specified, but no file path argument followed");
+    for (int i = 1; i < argc; ++i) {
+        if (std::strstr(argv[i], "=")) {
+            char* filename_ = std::strstr(argv[i], "=");
+            ++filename_;
+            if (std::strstr(argv[i], "of=") or std::strstr(argv[i], "outfile=")) {
+                rdp::output_filename = filename_;
+                std::cout << "Output file: " << filename_ << std::endl;
+            } else if (std::strstr(argv[i], "if=") or std::strstr(argv[i], "infile=")) {
+                input_file = filename_;
+                std::cout << "Input file: " << filename_ << std::endl;
             } else {
-                std::cout << "File: " << argv[2] << std::endl;
-                std::ifstream file(argv[2]);
-                while (std::getline(file, expression_line)) {
-                    rdp::parse(expression_line);
-                }
+                throw std::invalid_argument("invalid argument specified: '" + std::string(argv[i]) + "'");
             }
         } else {
-            throw std::invalid_argument("invalid argument specified: '" + std::string(argv[1]) + "'");
+            throw std::invalid_argument("invalid argument specified: '" + std::string(argv[i]) + "'");
+        }
+    }
+
+    if (input_file != nullptr) {
+        std::ifstream file(input_file);
+        while (std::getline(file, expression_line)) {
+            rdp::parse(expression_line);
         }
     } else {
         while (true) {
-            std::cout << std::endl << "Enter arithmetic expression: ";
+            std::cout << std::endl << "(q, quit, or exit) || Enter arithmetic expression: ";  // add exit options
             if (!std::getline(std::cin, expression_line))
                 return -1; // I/O error
             if (expression_line == "q" or expression_line == "quit" or expression_line == "exit")
                 break;
-            if (!expression_line.empty()) {
+            if (!expression_line.empty())
                 rdp::parse(expression_line);
-            }
         }
     }
     return 0;
